d373271782d97816c04e8b149c2f44d5edd0f1fe
paulacarvani <3312@holbertonschool.com>
2022-03-08 14:57:55 -0500

    Implement the cryptoJS for encrypt or decrypt the user password

--- API/routes/auth.js
+++ API/routes/auth.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const router = require("express").Router();
+const CryptoJS = require("crypto-js");
 
 
 //REGISTER
@@ -7,7 +8,9 @@ router.post("/register", async (req, resp) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        //encriptar el password
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC
+            ).toString(),
     });
 
     try{
@@ -18,6 +21,27 @@ router.post("/register", async (req, resp) => {
     }
 });
 
+//LOGIN
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        !user && res.status(401).json("Wrong credentials!");
+
+        //Desencriptar el password
+        const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
+        const Originalpassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+        Originalpassword !==req.body.password &&
+            res.status(401).json("Wrong credentials!");
+
+        const { password, ...others } = user._doc;
+
+        res.status(200).json(others);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 module.exports = router
