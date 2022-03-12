db69c6ea3a881a455a87b5ddb22413c7e4fa0702
paulacarvani <3312@holbertonschool.com>
2022-03-11 21:45:36 -0500

    Creates method CREATE for the products

--- API/routes/product.js
+++ API/routes/product.js
@@ -1,4 +1,25 @@
 const router = require("express").Router();
+const Product = require("../models/Product");
+const {
+    verifyToken,
+    verifyTokenAndAuthorization,
+    verifyTokenAndAdmin,
+} = require("./verifyToken");
+
+//CREATE
+
+router.post("/", verifyTokenAndAdmin, async (req, res) => {
+    const newProduct = new Product (req.body)
+
+    try {
+        const savedProduct = await newProduct.save();
+        res.status(200).json(savedProduct);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
+
+});
 
 
 
