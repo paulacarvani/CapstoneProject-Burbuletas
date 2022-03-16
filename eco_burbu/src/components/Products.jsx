18394396beb650970edcf3b689cb437ee1c132ee
paulacarvani <3312@holbertonschool.com>
2022-03-15 22:42:06 -0500

    Fixed padding spaces for sliders and products

--- eco_burbu/src/components/Products.jsx
+++ eco_burbu/src/components/Products.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import Product from "./Product"
 
 const Container = styled.div`
-    padding: 11px;
+    padding: 0px;
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
