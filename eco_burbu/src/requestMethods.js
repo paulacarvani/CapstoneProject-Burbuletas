8620c01aad3f8136fc86370cb288c8b349638c43
paulacarvani <3312@holbertonschool.com>
2022-03-20 15:02:45 -0500

    Update the Base url for the API connections to the server domain

--- eco_burbu/src/requestMethods.js
+++ eco_burbu/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/"
+const BASE_URL = "http://burbuletas.studio:5000/api/"
 //const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMzQwYzczMjllYjVhZDJlODcxZDExYSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NzU3ODI5MiwiZXhwIjoxNjQ3ODM3NDkyfQ.-fcgeBwkVGOWFLQTBmL3QDPvS9geLMIKR6kSb4I1q_M"
 
 
