34e875489337c7d2bc389dadee357f96979212d5
paulacarvani <3312@holbertonschool.com>
2022-03-20 01:14:20 -0500

    Update .jsx to js in all files

---
+++ eco_burbu/src/pages/Product.js
@@ -0,0 +1,116 @@
+import styled from "styled-components"
+import Footer from "../components/Footer"
+import Navbar1 from "../components/Navbar1"
+import Announcement from "../components/Announcement"
+import { mobile } from "../responsive"
+import { Link, useLocation } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { publicRequest } from "../requestMethods"
+
+const Container = styled.div`
+
+`;
+
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+  ${mobile({ flexDirection: "column" })}
+`;
+
+const ImgContainer = styled.div`
+  flex: 1;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  object-fit: cover;
+  ${mobile({ width: "100%" })}
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+`;
+
+const Title = styled.h1`
+  font-style: italic;
+`;
+
+const Desc = styled.p`
+  margin: 20px 0px;
+  font-weight: 100;
+  font-size: 18px;
+`;
+
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+
+const AddContainer = styled.div`
+  padding-top: 50px;
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  ${mobile({ width: "100%" })}
+`;
+
+const Button = styled.button`
+  padding: 15px;
+  border: 2px solid #aed79c;
+  background-color: white;
+  cursor: pointer;
+  font-weight: 500;
+  &:hover{
+      background-color: #f8f4f4;
+  }
+`;
+
+const Product = () => {
+  const location = useLocation();
+  const id = location.pathname.split("/")[2];
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const getProduct = async () => {
+      try {
+        const res = await publicRequest.get("/products/find/" + id);
+        setProduct(res.data);
+      } catch { }
+    };
+    getProduct();
+  }, [id]);
+
+
+
+  return (
+    <Container>
+      <Navbar1 />
+      <Announcement />
+      <Wrapper>
+        <ImgContainer>
+          <Image src={product.image}/>
+        </ImgContainer>
+        <InfoContainer>
+          <Title>
+            {product.name}
+          </Title>
+          <Desc>
+            {product.description}
+          </Desc>
+          <Price>
+            {product.price}
+          </Price>
+          <AddContainer>
+            <Button>
+              Añadir a Favoritos
+            </Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
+      <Footer />
+    </Container>
+  )
+}
+export default Product
--- eco_burbu/src/store/index.js
+++ eco_burbu/src/store/index.js
@@ -1,10 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { reducerModal } from "./modalSlide";
-import favoriteReducer from "./Favorite";
+
 
 export const store = configureStore ({
   reducer: {
     modal: reducerModal,
-    favorite: favoriteReducer,
   }
 });
