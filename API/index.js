e8ae3003a4f8cb4514b39198a7d46c50eb274ea0
paulacarvani <3312@holbertonschool.com>
2022-03-23 22:24:51 -0500

    Update the backend api with ssl certified and https

--- API/index.js
+++ API/index.js
@@ -6,8 +6,12 @@ const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cors = require("cors")
+const fs = require("fs");
+const https = require("https");
 
 dotenv.config();
+
+const PORT = 5000;
 
 mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("DBConnection Succesfull!"))
@@ -22,6 +26,13 @@ app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 
 
-app.listen(process.env.PORT || 5000, () => {
+/* app.listen(process.env.PORT || 5000, () => {
     console.log("Backend Server is Running!");
 });
+*/
+https.createServer({
+    cert: fs.readFileSync('mi_certificado.crt'),
+    key: fs.readFileSync('mi_certificado.pem')
+  },app).listen(PORT, function(){
+     console.log('Servidor https correindo en el puerto 443');
+ });
